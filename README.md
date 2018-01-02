907b439e64909df30af7695ba48291c3fd606e23
Robert Basden <rob.basden@gmail.com>
2018-01-02 15:43:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Robert Basden's Dotfiles
+
+## Installation
+
+```sh
+$ git clone https://github.com/robertbasden/dotfiles.git ~/dotfiles
+$ cd ~/dotfiles
+$ chmod +x setup.sh
+$ ./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+# This symlinks all the dotfiles  to ~/
+# This is safe to run multiple times and will prompt you about anything unclear
+
+#
+# Utils
+#
+
+answer_is_yes() {
+  [[ "$REPLY" =~ ^[Yy]$ ]] \
+    && return 0 \
+    || return 1
+}
+
+ask() {
+  print_question "$1"
+  read
+}
+
+ask_for_confirmation() {
+  print_question "$1 (y/n) "
+  read -n 1
+  printf "\n"
+}
+
+ask_for_sudo() {
+
+  # Ask for the administrator password upfront
+  sudo -v
+
+  # Update existing `sudo` time stamp until this script has finished
+  # https://gist.github.com/cowboy/3118588
+  while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+  done &> /dev/null &
+
+}
+
+cmd_exists() {
+  [ -x "$(command -v "$1")" ] \
+    && printf 0 \
+    || printf 1
+}
+
+execute() {
+  $1 &> /dev/null
+  print_result $? "${2:-$1}"
+}
+
+get_answer() {
+  printf "$REPLY"
+}
+
+get_os() {
+
+  declare -r OS_NAME="$(uname -s)"
+  local os=""
+
+  if [ "$OS_NAME" == "Darwin" ]; then
+    os="osx"
+  elif [ "$OS_NAME" == "Linux" ] && [ -e "/etc/lsb-release" ]; then
+    os="ubuntu"
+  fi
+
+  printf "%s" "$os"
+
+}
+
+is_git_repository() {
+  [ "$(git rev-parse &>/dev/null; printf $?)" -eq 0 ] \
+    && return 0 \
+    || return 1
+}
+
+mkd() {
+  if [ -n "$1" ]; then
+    if [ -e "$1" ]; then
+      if [ ! -d "$1" ]; then
+        print_error "$1 - a file with the same name already exists!"
+      else
+        print_success "$1"
+      fi
+    else
+      execute "mkdir -p $1" "$1"
+    fi
+  fi
+}
+
+print_error() {
+  # Print output in red
+  printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
+print_info() {
+  # Print output in purple
+  printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+print_question() {
+  # Print output in yellow
+  printf "\e[0;33m  [?] $1\e[0m"
+}
+
+print_result() {
+  [ $1 -eq 0 ] \
+    && print_success "$2" \
+    || print_error "$2"
+
+  [ "$3" == "true" ] && [ $1 -ne 0 ] \
+    && exit
+}
+
+print_success() {
+  # Print output in green
+  printf "\e[0;32m  [✔] $1\e[0m\n"
+}
+
+# Warn user this script will overwrite current dotfiles
+while true; do
+  read -p "Warning: this will overwrite your current dotfiles. Continue? [y/n] " yn
+  case $yn in
+    [Yy]* ) break;;
+    [Nn]* ) exit;;
+    * ) echo "Please answer yes or no.";;
+  esac
+done
+
+# Get the dotfiles directory's absolute path
+SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
+DOTFILES_DIR="$(dirname "$SCRIPT_DIR")"
+
+
+dir=~/dotfiles                        # dotfiles directory
+dir_backup=~/dotfiles_old             # old dotfiles backup directory
+
+# Get current dir (so run this script from anywhere)
+
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Create dotfiles_old in homedir
+echo -n "Creating $dir_backup for backup of any existing dotfiles in ~..."
+mkdir -p $dir_backup
+echo "done"
+
+# Change to the dotfiles directory
+echo -n "Changing to the $dir directory..."
+cd $dir
+echo "done"
+
+#
+# Actual symlink stuff
+#
+
+declare -a FILES_TO_SYMLINK=(
+  'git/gitconfig'
+  'git/gitignore'
+)
+
+# Move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+
+echo "Moving any existing dotfiles from ~ to $dir_backup"
+for i in ${FILES_TO_SYMLINK[@]}; do
+  mv ~/.${i##*/} ~/dotfiles_old/
+done
+
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+  local i=''
+  local sourceFile=''
+  local targetFile=''
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  for i in ${FILES_TO_SYMLINK[@]}; do
+
+    sourceFile="$(pwd)/$i"
+    targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+
+    if [ ! -e "$targetFile" ]; then
+      execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+    elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
+      print_success "$targetFile → $sourceFile"
+    else
+      ask_for_confirmation "'$targetFile' already exists, do you want to overwrite it?"
+      if answer_is_yes; then
+        rm -rf "$targetFile"
+        execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
+      else
+        print_error "$targetFile → $sourceFile"
+      fi
+    fi
+
+  done
+
+  unset FILES_TO_SYMLINK
+
+}
+
+main
